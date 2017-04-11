1809b98a1c17d07cfeda63e0976235976c7c1a79
Michał Pomarański <michal.pomaranski@gmail.com>
2017-04-11 23:06:49 +0200

    removed other frontend folder

--- src/main/clojurescript-chores/env/prod/cljs/clojurescript_chores/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns clojurescript-chores.prod
-  (:require [clojurescript-chores.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
--- src/main/clojurescript-chores/src/clj/clojurescript_chores/server.clj
+++
@@ -1,9 +0,0 @@
-(ns clojurescript-chores.server
-  (:require [clojurescript-chores.handler :refer [app]]
-            [config.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
-  (:gen-class))
-
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty app {:port port :join? false})))
